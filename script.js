b57707e6f200b82693ff0333e22a6bd123d6148d
HASSAN Ibrahim <mhassan.ibrahim94@gmail.com>
2021-02-01 16:55:09 +0100

    correction code + ajout bouton volume

---
+++ script.js
@@ -0,0 +1,62 @@
+var playPauseBtn = document.querySelector('.playpause');
+var stopBtn = document.querySelector('.stop');
+var rwdBtn = document.querySelector('.rwd');
+var fwdBtn = document.querySelector('.fwd');
+var timeLabel = document.querySelector('.time');
+var player = document.querySelector('video');
+var volume = document.querySelector('vol');
+
+playPauseBtn.onclick = function() {
+    if(player.paused) {
+      player.play();
+      playPauseBtn.textContent = 'Pause';
+    } else {
+      player.pause();
+      playPauseBtn.textContent = 'Play';
+    }
+  };
+
+  stopBtn.onclick = function() {
+    player.pause();
+    player.currentTime = 0;
+    playPauseBtn.textContent = 'Play';
+  };
+
+  rwdBtn.onclick = function() {
+    player.currentTime -= 5;
+  };
+  
+  fwdBtn.onclick = function() {
+    player.currentTime += 5;
+    if(player.currentTime >= player.duration || player.paused) {
+      player.pause();
+      player.currentTime = 0;
+      playPauseBtn.textContent = 'Play';
+    }
+  };
+
+  player.ontimeupdate = function() {
+    var minutes = Math.floor(player.currentTime / 60);
+    var seconds = Math.floor(player.currentTime - minutes * 60);
+    var minuteValue;
+    var secondValue;
+  
+    if (minutes<10) {
+      minuteValue = "0" + minutes;
+    } else {
+      minuteValue = minutes;
+    }
+  
+    if (seconds<10) {
+      secondValue = "0" + seconds;
+    } else {
+      secondValue = seconds;
+    }
+  
+    mediaTime = minuteValue + ":" + secondValue;
+    timeLabel.textContent = mediaTime;
+  };
+
+  volume.volumechange = function() {
+    
+  }
